42afb6915eff0323fa87760fdc1b61f97ba19164
kouui <lelouchkako@gmail.com>
2019-07-30 01:26:37 +0900

    ...

---
+++ m2wr_robot/src/publish_world_to_base_tf.cpp
@@ -0,0 +1,59 @@
+
+#include <ros/ros.h>
+#include <gazebo_msgs/ModelStates.h>
+#include <tf/transform_broadcaster.h>
+
+#include <string>
+#include <math.h>
+
+gazebo_msgs::ModelStates last_model_msg;
+
+void models_callback(const gazebo::ModelStates& model_msg)
+{
+    last_model_msg = model_msg;
+}
+
+int main(int argc, char const *argv[])
+{
+    ros::init(argc, argv, "publish_world_to_base");
+    ros::NodeHandle nh;
+    ros::NodeHandle pnh("~");
+
+    std::string model_name  = "";
+    std::string world_frame = "";
+    std::string base_frame  = "";
+
+    float hz = 20.;
+
+    // rosparam
+    pnh.getParam("model_name", model_name);
+    pnh.getParam("world_frame", world_frame);
+    pnh.getParam("base_frame", base_frame);
+    pnh.getParam("hz", hz);
+
+    // subscribe
+    ros::subscriber joy_sub = nh.subscibe("/gazebo/model_states", 10, models_callback);
+
+    // get and pubish tf
+    tf::TransformBroadcaster br;
+    ros::Rate loop_rate(hz);
+    while (ros::ok)
+    {
+        auto model_size = last_model_msg.name.size();
+
+        for (auto i=0; i<model_size; i++)
+        {
+            if (last_model_msg.name[i] == model_name)
+            {
+                tf::Transform transform;
+                tf::poseMsgToTF(last_model_msg.pose[i], transform);
+                br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), world_frame, base_frame));
+            }
+        }
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
